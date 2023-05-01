0534cb9eb3a5cf39e8ed60f1a91d46cc99f5afe5
jakeseo <n00nietzsche@gmail.com>
2023-05-01 14:42:37 +0900

    first commit

---
+++ src/main/java/com/study/effectivesoftwaretesting/EffectiveSoftwareTestingApplication.java
@@ -0,0 +1,13 @@
+package com.study.effectivesoftwaretesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveSoftwareTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveSoftwareTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/effectivesoftwaretesting/EffectiveSoftwareTestingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.study.effectivesoftwaretesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveSoftwareTestingApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
